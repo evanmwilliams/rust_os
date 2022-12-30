fb1b9c8619825be937b6786848d707ceec7a1d8a
Evan Williams <emw236@cornell.edu>
2022-12-30 01:25:59 -0500

    support for huge pages

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,17 @@ use rust_os::println;
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use rust_os::memory::active_level_4_table;
-    use rust_os::memory::translate_addr;
-    use x86_64::VirtAddr;
+    use rust_os::memory;
+    use x86_64::{structures::paging::Translate, VirtAddr};
 
     println!("Hello World{}", "!");
     rust_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe { memory::init(phys_mem_offset) };
 
     let addresses = [
-        // the identity-mapped vga buffer page
+        // the identity mapped vga buffer page
         0xb8000,
         // some code page
         0x201008,
@@ -33,7 +33,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     for &address in &addresses {
         let virt = VirtAddr::new(address);
-        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        let phys = mapper.translate_addr(virt);
         println!("{:?} -> {:?}", virt, phys);
     }
 
